be007f4b916539fe78e887d104943d3209c2e3fe
Jim Schrempp <github1@jimschrempp.com>
2020-01-07 16:29:55 -0800

    correct dev keys

--- checkinfirmware/src/rfidkeys.cpp
+++ checkinfirmware/src/rfidkeys.cpp
@@ -6,8 +6,6 @@
 //  Replace the text blocks below with the six byte values you use for your keys.
 //  Of the form: [160,141,167,49,73,21]
 //
-#include "rfidkeys.h"
 #include "application.h"
 
-// keys used to write and read a card
-String RFIDKeysJSON = "{\"WriteKey\":[91,127,197,134,179,14],\"ReadKey\":[29,97,96,199,174,204],\"WriteKeyOld\":[160,161,162,163,164,165],\"ReadKeyOld\":[176,177,178,179,180,181] }";
+String RFIDKeysJSON = "{\"WriteKey\":[100,101,102,103,104,105],\"ReadKey\":[200,201,202,203,205,205] }";
